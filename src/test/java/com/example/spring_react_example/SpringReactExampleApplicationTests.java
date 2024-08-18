03c67bd40cbd818b9c9a2de7f4dad21be5be3805
fermarino <119690095+fermarino@users.noreply.github.com>
2024-08-18 20:40:21 -0300

    first commit

---
+++ src/test/java/com/example/spring_react_example/SpringReactExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_react_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
